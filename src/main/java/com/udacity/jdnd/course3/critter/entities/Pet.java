46e6df5fe5b755806a706cb31d4dcc5a955652b8
Ryan Wilcox <rwilcox11@icloud.com>
2020-10-04 15:45:40 -0400

    Updated project structure and created entities

---
+++ src/main/java/com/udacity/jdnd/course3/critter/entities/Pet.java
@@ -0,0 +1,70 @@
+package com.udacity.jdnd.course3.critter.entities;
+
+import com.udacity.jdnd.course3.critter.pet.PetType;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private PetType petType;
+
+  @Nationalized
+  private String name;
+
+  private LocalDate birthDate;
+
+  @Column(length = 500)
+  private String notes;
+
+  @ManyToOne(targetEntity = Customer.class)
+  private Customer customer;
+
+  public Pet(){}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public PetType getPetType() {
+    return petType;
+  }
+
+  public void setPetType(PetType petType) {
+    this.petType = petType;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+}
