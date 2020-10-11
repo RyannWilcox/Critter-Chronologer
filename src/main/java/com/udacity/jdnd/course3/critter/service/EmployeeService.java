4663d099428f8870ef9b9e3fad518a2d1dedcec2
Ryan Wilcox <rwilcox11@icloud.com>
2020-10-11 17:05:44 -0400

    Added transactional to all services per review comments. Transactional annotations should be added to these services because they are requests items from the database.

--- src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/EmployeeService.java
@@ -5,11 +5,13 @@ import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.DayOfWeek;
 import java.util.List;
 
 
 @Service
+@Transactional
 public class EmployeeService {
 
   @Autowired
